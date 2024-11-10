81edc54c83db07d03fbd148973c634224d02c813
Thomas Van <36116384+tinbolw@users.noreply.github.com>
2024-11-10 11:54:05 -0800

    updated files, add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# A barebones Discord bot boilerplate. 
+
+The majority of the code is taken verbatim from the discord.js [official guide](https://discordjs.guide/). The only meaningful change in this is that [dotenv](https://www.npmjs.com/package/dotenv) is used instead of a local config file. Thus, the only dependencies are [discord.js](https://www.npmjs.com/package/discord.js) and the aforementioned.
+
+For further details, read the [official guide](https://discordjs.guide/).
+
+I am not affiliated, associated, authorized, endorsed by, or in any way officially connected with Discord or discord.js.
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,7 @@
 const { REST, Routes } = require('discord.js');
-require('dotenv').config()
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
-const token = process.env.TOKEN;
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
@@ -28,7 +25,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -37,7 +34,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
 
