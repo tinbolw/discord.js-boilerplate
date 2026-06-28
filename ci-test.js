44448f4a2be0b0b89853a7e3eb31a98cb644b503
Thomas Van <tinbolw@gmail.com>
2026-06-28 11:16:30 -0700

    add test script

---
+++ ci-test.js
@@ -0,0 +1,47 @@
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    client.destroy();
+    process.exit(0);
+});
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
+async function main() {
+    client.login(process.env.TOKEN);
+    await sleep(15000);
+    console.log('Bot failed to login');
+    client.destroy();
+    process.exit(1);
+}
+
+main();
